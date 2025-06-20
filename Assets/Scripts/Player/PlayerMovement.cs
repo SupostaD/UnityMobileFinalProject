a0d06602706e86b9ae624b5f51df6b318b18b9d0
Maksim Zavarin <supostad@gmail.com>
2025-06-20 23:09:57 +0300

    player assets + animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody Rb;
     public PlayerRoll PlayerRoll;
     public ControllSchemePlayerManager ControllScheme;
+    public Animator animator;
     private IPlayerInput playerInput;
     private Vector3 moveDirection;
+    
 
     private void Start()
     {
@@ -27,9 +29,12 @@ public class PlayerMovement : MonoBehaviour
 
         if (moveDirection.sqrMagnitude > 0.01f)
         {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isShooting", false);
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
             transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 0.2f);
         }
+        else animator.SetBool("isRunning", false);
     }
 
     void FixedUpdate()
